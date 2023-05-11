46362fee38d00e4b6c79cdd4db0416a52743dfaf
Gorjan Spirovski <gorjanspirovski@gmail.com>
2023-05-11 17:15:52 +0200

    bux fix

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('', views.index, name='/'),
     path('index/', views.index, name='index'),
     path('logout/', LogoutView.as_view(), name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
